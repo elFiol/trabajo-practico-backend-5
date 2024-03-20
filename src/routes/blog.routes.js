9a4e6f5c7a88dfd03d403d0cbed25d1b95dfe27b
Fiol Thiago <fiolthiago9@gmail.com>
2024-03-19 22:55:43 -0300

    arreglamos la funcion de crearUsuario

--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -1,10 +1,10 @@
 import { Router } from "express";
-import { listarRecetas, crearRecetas, borrarRecetas, editarRecetas, agregarUsuario } from "../controller/recetas.controller.js";
+import { listarRecetas, crearRecetas, borrarRecetas, editarRecetas, crearUsuarios } from "../controller/recetas.controller.js";
 
 const Recetasrouter = Router();
 
 Recetasrouter.route('/recetas').get(listarRecetas).post(crearRecetas);
 Recetasrouter.route('/receta/:id').delete(borrarRecetas).put(editarRecetas);
-Recetasrouter.route('/usuario').post(agregarUsuario)
+Recetasrouter.route('/usuario').post(crearUsuarios)
 
 export default Recetasrouter
