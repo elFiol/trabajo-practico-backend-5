9cb6831cf941c4c53c247eaca793ba70e8c47c23
Fiol Thiago <fiolthiago9@gmail.com>
2024-03-19 22:36:05 -0300

    añadimos la funcion de editar

--- src/controller/recetas.controller.js
+++ src/controller/recetas.controller.js
@@ -1,17 +1,19 @@
+import Recetas from "../database/model/recetas.js"
+import Usuario from "../database/model/usuarios.js"
 
-
-export const editarColor = async(req, res) => {
+export const editarRecetas = async(req, res) => {
     try{
-        const buscarColor = await Color.findById(req.params.id)
-        if(!buscarColor){
-            return res.status(404).json({mensaje: "no se pudo editar el color, el id es incorrecto"})
+        const buscarRecetas = await Recetas.findById(req.params.id)
+        if(!buscarRecetas){
+            return res.status(404).json({mensaje: "no se pudo editar la receta, el id es incorrecto"})
         }
-        await Color.findByIdAndUpdate(req.params.id, req.body)
-        res.status(200).json({mensage: "el color fue modificado exitosamente"})
+        await Recetas.findByIdAndUpdate(req.params.id, req.body)
+        res.status(200).json({mensage: "la receta fue modificado exitosamente"})
     }catch(error){
         console.log(error)
         res.status(500).json({
-            mensaje: "ocurrio un error al querer intentar editar el color"
+            mensaje: "ocurrio un error al querer intentar editar la recetas"
         })
     }
 }
+
--- src/routes/blog.routes.js
+++ src/routes/blog.routes.js
@@ -4,5 +4,6 @@ const Recetasrouter = Router();
 
 Recetasrouter.route('/recetas').get(listarRecetas).post(crearRecetas);
 Recetasrouter.route('/receta/:id').delete(borrarRecetas).put(editarRecetas);
+Recetasrouter.route('/usuario').post(agregarUsuario)
 
 export default Recetasrouter
