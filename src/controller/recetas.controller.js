0983b7b1b9b598f1c8727d947d7b2c60a650a235
Fiol Thiago <fiolthiago9@gmail.com>
2024-03-19 22:43:11 -0300

    añadimos la funcion de borrar

--- src/controller/recetas.controller.js
+++ src/controller/recetas.controller.js
@@ -30,7 +30,25 @@ export const crearRecetas = async (req, res) => {
         mensaje: "No se pudo procesar la solicitud de crear la receta",
       });
     }
-}; 
+};
+
+export const borrarRecetas = async (req, res) => {
+    try {
+      const buscarRecetas = await Recetas.findById(req.params.id);
+      if (!buscarRecetas) {
+        return res
+          .status(404)
+          .json({ mensaje: "no se pudo borrar la receta, el id es incorrecto" });
+      }
+      await Recetas.findByIdAndDelete(req.params.id);
+      res.status(200).json({ mensage: "la receta fue borrada exitosamente" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({
+        mensaje: "ocurrio un error al querer intentar borrar la receta",
+      });
+    }
+};
 
 export const crearUsuarios = async (req, res) => {
     try {
